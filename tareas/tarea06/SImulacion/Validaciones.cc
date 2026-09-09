3064a771899943763610263cd0c7f43a0de8eec8
Meli290404 <melgach2904@gmail.com>
2026-09-08 21:50:05 -0600

    Se agrega la validación para el count

--- tareas/tarea06/SImulacion/Validaciones.cc
+++ tareas/tarea06/SImulacion/Validaciones.cc
@@ -10,3 +10,8 @@ bool validarProducto( const std::string & valor ) {
     static const std::regex patron( "^[a-zA-Z0-9\\-]{1,50}$" );
     return std::regex_match( valor, patron );
 }
+
+bool validarCount( const std::string & valor ) {
+    static const std::regex patron( "^[0-9]{1,3}$" );
+    return std::regex_match( valor, patron );
+}
--- tareas/tarea06/SImulacion/Validaciones.h
+++ tareas/tarea06/SImulacion/Validaciones.h
@@ -5,5 +5,6 @@
 
 bool validarCategoria( const std::string & valor ); // ^[a-zA-Z]{3,20}$
 bool validarProducto( const std::string & valor );  // ^[a-zA-Z0-9\-]{1,50}$
+bool validarCount( const std::string & valor );     // ^[0-9]{1,3}$
 
 #endif
